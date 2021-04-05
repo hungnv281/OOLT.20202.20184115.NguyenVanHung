d3f1f7521f251c78e480807edbf06e5506a8d24a
hungnv281 <71517407+hungnv281@users.noreply.github.com>
2021-04-05 22:13:10 +0700

    Add files via upload

---
+++ lab05/hust/soict/hedspi/garbage/ConcatenationInLoops.java
@@ -0,0 +1,13 @@
+package hust.soict.hedspi.garbage;
+
+import java.util.Random;
+
+public class ConcatenationInLoops {
+	public static void main(String[] args) {
+		GarbageCreator a = new GarbageCreator();
+		NoGarbage b = new NoGarbage();
+		
+		
+	}
+
+}
---
+++ lab05/hust/soict/hedspi/garbage/GarbageCreator.java
@@ -0,0 +1,17 @@
+package hust.soict.hedspi.garbage;
+
+import java.util.Random;
+
+public class GarbageCreator {
+
+	public static void main(String[] args) {
+		Random r = new Random(123);
+		long start =  System.currentTimeMillis();
+		String s = "";
+		for (int i = 0; i < 9999999 ; i++)
+			s += r.nextInt(2);
+		System.out.println(System.currentTimeMillis()-start); // 
+
+	}
+
+}
