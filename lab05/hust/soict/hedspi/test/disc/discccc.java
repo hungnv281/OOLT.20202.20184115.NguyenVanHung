0174133644977cfdc1cab16f2a11bdb215952811
= <=>
2021-04-05 15:17:56 +0700

    hi

---
+++ lab05/hust/soict/hedspi/test/disc/discccc.java
@@ -0,0 +1,18 @@
+package hust.soict.hedspi.test.disc;
+
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class discccc {
+	DigitalVideoDisc disc;
+	discccc(DigitalVideoDisc disc) {
+		this.disc = disc;
+	}
+	public DigitalVideoDisc getDisc() {
+		return disc;
+	}
+	public void setDisc(DigitalVideoDisc disc) {
+		this.disc = disc;
+	}
+	
+
+}
