90535806bbca60b29097a1de77b4c90c8493a463
= <=>
2021-04-06 20:42:29 +0700

    comment

---
+++ lab06/src/hust/soict/hedspi/aims/Aims.java
@@ -0,0 +1,12 @@
+package hust.soict.hedspi.aims;
+
+import hust.soict.globalict.aims.media.DigitalVideoDisc;
+import hust.soict.hedspi.aims.order.Order;
+
+public class Aims {
+    public static void main(String[] args) {
+        Order anOrder = new Order();
+        
+    }
+}
+
